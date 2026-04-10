d110853eabae7088249715f66eb8d8759d18b0c4
khangdasac <145788969+khangdasac@users.noreply.github.com>
2026-04-10 13:51:44 +0700

    tuan0= 07

---
+++ Tuan07/auth-service/src/module/user/user.controller.ts
@@ -0,0 +1,7 @@
+import { Controller, Post, Body, Get } from "@nestjs/common";
+import { UserService } from "./user.service";
+
+@Controller('user')
+export class UserController {
+    constructor(private userService: UserService) { }
+}
---
+++ Tuan07/auth-service/src/module/user/user.service.ts
@@ -0,0 +1,38 @@
+import { Injectable, BadRequestException, UnauthorizedException } from "@nestjs/common";
+import { MariaService } from "../../prisma/maria/maria.service";
+import { User } from "../../../generated/maria"; 
+import { RegisterDto } from "../auth/dto/registerDto";
+
+@Injectable()
+export class UserService {
+    constructor(
+        private readonly mariaService: MariaService
+    ) { }
+
+    async findOne(userId: number): Promise<User | null> {
+        return this.mariaService.user.findUnique({ where: { id: userId } });
+    }
+
+    async findOneByUsername(username: string): Promise<User | null> {
+        return this.mariaService.user.findUnique({ where: { username } });
+    }
+
+    async findOneByEmail(email: string): Promise<User | null> {
+        return this.mariaService.user.findUnique({ where: { email } });
+    }
+
+    async create(registerDto: RegisterDto): Promise<User> {
+        const existingUser = await this.findOneByEmail(registerDto.email);
+        if (existingUser) {
+            throw new BadRequestException('Email already exists');
+        }
+        return this.mariaService.user.create({
+            data: {
+                email: registerDto.email,
+                password: registerDto.password,
+                username: registerDto.username,
+                role: 'USER',
+            },
+        });
+    }
+}
